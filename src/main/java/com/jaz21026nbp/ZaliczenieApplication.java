2ee9abfeb6631475284b36a9faa6b37143c30d98
s21026-pj <56227325+s21026-pj@users.noreply.github.com>
2021-07-03 16:11:51 +0200

    Initial commit

---
+++ src/main/java/com/jaz21026nbp/ZaliczenieApplication.java
@@ -0,0 +1,13 @@
+package com.jaz21026nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZaliczenieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZaliczenieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jaz21026nbp/ZaliczenieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaz21026nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZaliczenieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
